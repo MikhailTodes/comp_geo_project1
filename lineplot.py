3f3909748d7d3fd24f717a4f20984182af44de66
MikhailTodes <mikhailtodes2016@u.northwestern.edu>
2015-10-19 11:47:44 -0500

    Add bresnan algorithm for different cases of plotting

--- lineplot.py
+++ lineplot.py
@@ -3,42 +3,52 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-points = [[2,2],[3,3],[6,8],[7,2],[3,2]]
+points = [(150,300),(450,300),(570,525),(653,780),(495,845),(350,740),(100,345)]
+points.append (points[0])#add the first point onto the end to close the hull
 
-for i in range (len(points)-1):
-    if float(((points[i+1][0])-(points[i][0]))) == 0:
+for i in range (len(points)-1):#Looping through each pair of point in the list
+    p1 = points [i]#Save the first point for comparison into var p1
+    p2 = points [i+1]#Save the second point for comparison into var p2
+    print (p1  , p2)
+
+    #----Make sure always plotting from left to right-----
+    if(p1[0]>p2[0]):
+        temp = p2
+        p2 = p1
+        p1 = temp
+    #_____________________________________________________
+
+    if float(((p1[0])-(p2[0]))) == 0:#Checking for a zero valued slope
         m=0.0
-        print("ZeroDiv")
+        print("ZeroDiv")#debugging purposes
     else:
-        m = ((points[i+1][1])-(points[i][1]))/float(((points[i+1][0])-(points[i][0])))
-    c = points[i][1]-m*points[i][0]
+        m = ((p2[1])-(p1[1]))/float(((p2[0])-(p1[0])))#Calculating the slope of the line
+    c = p1[1]-m*p1[0]#Calculating the intercept
+
+    #debugging purposes
     print (i)
     print (m)
     print (c)
     print
 
-    x = np.arange(points[i][0], points[i+1][0], 0.01)
+    #Cases with both positive and negative slopes but -1 < slope < 1 (plotting is the same)
+    #Cases printed in green
+    if (m>-1.0 and m<=1.0):
+        step = np.arange(p1[0], p2[0], 1)
+        plt.plot(step, m*step + c, 'g,')
 
-    # red dashes, blue squares and green triangles
-    plt.plot(x, m*x + c, 'g,')
-    plt.axis([0, 11, 0, 11])
+    #Case positive slop and slope > 1 (plotting is the same)
+    #case printed in red
+    if (m>1.0):
+        step = np.arange(p1[1], p2[1], 1)
+        plt.plot((step-c)/m, step, 'r,')
 
-if float(((points[0][0])-(points[len(points)-1][0]))) == 0:
-    m=0.0
-    print("ZeroDiv")
-else:
-    m = ((points[0][1])-(points[len(points)-1][1]))/float(((points[0][0])-([len(points)-1][0])))
-c = points[0][1]-m*points[0][0]
-print (m)
-print (c)
-print
+    #Case negative slope and slope < -1 (plotting is the same)
+    #case printed in blue
+    if (m<-1.0):
+        step = np.arange(p2[1], p1[1], 1)
+        plt.plot((step-c)/m, step, 'b,')
 
-x = np.arange(points[0][0], points[len(points)-1][0], 0.01)
-
-# red dashes, blue squares and green triangles
-plt.plot(x, m*x + c, 'g,')
-plt.axis([0, 11, 0, 11])
-
-
+plt.axis([0, 1000, 0, 1000])
 plt.show()
 
