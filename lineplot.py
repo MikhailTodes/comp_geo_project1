69aa21e9efe11036e552f26a38805119a402d593
MikhailTodes <mikhailtodes2016@u.northwestern.edu>
2015-10-20 00:33:59 -0500

    Fixed convex hull algorithm Fixed reading from text file robustly Added text files to folder

--- lineplot.py
+++ lineplot.py
@@ -2,6 +2,8 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import re
+
 
 
 def plotting_points_into_shape(points):
@@ -34,7 +36,7 @@ def plotting_points_into_shape(points):
 
         #Cases with both positive and negative slopes but -1 < slope < 1 (plotting is the same)
         #Cases printed in green
-        if (m>-1.0 and m<=1.0):
+        if (m>=-1.0 and m<=1.0):
             step = np.arange(p1[0], p2[0], 1)
             plt.plot(step, m*step + c, 'g,')
 
@@ -43,6 +45,12 @@ def plotting_points_into_shape(points):
         if (m>1.0):
             if(m==999.0):
                 print("vertical line")
+                #----Make sure always plotting from bottom to top-----
+                if(p1[1]>p2[1]):
+                    temp = p2
+                    p2 = p1
+                    p1 = temp
+                #______________________________________________________
                 step = np.arange(p1[1], p2[1], 1)
                 plt.plot(step*0 + p1[0], step, 'r,')
             else:
@@ -55,11 +63,117 @@ def plotting_points_into_shape(points):
             step = np.arange(p2[1], p1[1], 1)
             plt.plot((step-c)/m, step, 'b,')
 
+
+
+
+
+
+
+
+def convex_hull(points):
+
+
+    # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
+    # Returns a positive value, if OAB makes a counter-clockwise turn,
+    # negative for clockwise turn, and zero if the points are collinear.
+    def cross(o, a, b):
+        return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+    # Build lower hull 
+    lower = []
+    for i in points:
+        while len(lower) >= 2 and cross(lower[-2], lower[-1], i) <= 0:
+            lower.pop()
+        lower.append(i)
+
+    # Build upper hull
+    upper = []
+    for i in reversed(points):
+        while len(upper) >= 2 and cross(upper[-2], upper[-1], i) <= 0:
+            upper.pop()
+        upper.append(i)
+ 
+    return lower[:-1] + upper[:-1]#Return the lower and upper hulls that were built
+                                  #(minus the last element which is repeated as the first element in each list)
+
+
+
+
+
+
+
+
+
+
 def main():
-    points1 = [(150,300),(150,400),(570,525),(653,780),(495,780),(350,740),(100,345)]
-    points2 = [(),(),(),(),(),()]
-    plotting_points_into_shape(points1)
-    plt.axis([0, 1000, 0, 1000])
+    points = []
+   
+    #__________OPENING A FILE WITH ROBUST CHECKING_____________
+    check = 0
+    
+    while (check == 0):
+        input_file = raw_input("\nPlease enter the name of the file you wish to open or q to quit:\n")
+        if (input_file == 'q'):
+            return 0
+        else:
+            try:
+                f = open(input_file, 'r')
+                #-----Read first line to check what kind of file-------
+                input_type = f.readline()
+                #check =1
+                input_type = input_type.strip('\r\n')
+                if (input_type == 'P' or input_type == 'S' or input_type == 'U'):
+                    check = 1
+                else:
+                    print ("\nFile specified is not correctly formatted")
+                #------------------------------------------------------
+                
+            except:
+                print ("\nCannot find specified file...")
+    #____________________________________________________________
+
+    if (input_type == 'P'):#P Case (Plot polygon given)
+        while (check==1):
+            try:
+                current = f.readline()
+                current = re.split('[(,)]', current)
+                temp = [int(current[1]), int(current[2])]
+                points.append(temp)
+            except:
+                check=0
+        print 'Points:\n', points
+        plotting_points_into_shape(points)
+
+
+
+    elif (input_type == 'U'):#U Case (Plotting intersection of 2 polygons)
+        print ("Ucase")
+
+
+
+    else:#S Case (Find Convex hull of given points)
+        while (check==1):
+            try:
+                current = f.readline()
+                current = re.split('[(,)]', current)
+                temp = [int(current[1]), int(current[2])]
+                points.append(temp)
+            except:
+                check=0
+        print 'Points:\n', points        
+        
+        #Make sure there is more that 1 point
+        if len(points) <= 1:
+            return 0
+        plotting_points_into_shape(points)
+        points = convex_hull(points)
+        plotting_points_into_shape(points)
+
+
+
+
+
+    plt.axis([0, 1100, 0, 1100])
     plt.show()
     
 
