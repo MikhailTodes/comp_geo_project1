06ed23bbbbdaf566ad0c539d7d3936322aa10122
MikhailTodes <mikhailtodes2016@u.northwestern.edu>
2015-10-21 23:10:59 -0500

    Try flood fill- add Input5.txt for small testing so recursion error is solved

--- lineplot.py
+++ lineplot.py
@@ -4,15 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import re
 
+plotmatrix = [[0]*1100 for i in range(1100)]#Matrix to keep track of plotted pixels
+floodfillcalls = 0
+
+def bres_check(a):#Rounding off right for bresnan algorthim
+    if ((a-int(a))*10>=5):
+        return int(a)+1
+    else:
+        return int(a)
+
+
 
 
 def plotting_points_into_shape(points):
+    global plotmatrix
     points.append (points[0])#add the first point onto the end to close the hull
 
     for i in range (len(points)-1):#Looping through each pair of point in the list
         p1 = points [i]#Save the first point for comparison into var p1
         p2 = points [i+1]#Save the second point for comparison into var p2
-        print (p1  , p2)
+        #print (p1  , p2)
 
         #----Make sure always plotting from left to right-----
         if(p1[0]>p2[0]):
@@ -29,49 +40,75 @@ def plotting_points_into_shape(points):
         c = p1[1]-m*p1[0]#Calculating the intercept
 
         #debugging purposes
-        print (i)
-        print (m)
-        print (c)
-        print
+        #print (i)
+        #print (m)
+        #print (c)
+        #print
 
         #Cases with both positive and negative slopes but -1 < slope < 1 (plotting is the same)
         #Cases printed in green
         if (m>=-1.0 and m<=1.0):
-            step = np.arange(p1[0], p2[0], 1)
-            plt.plot(step, m*step + c, 'g,')
+            #step = np.arange(p1[0], p2[0], 1)
+            for i in range (p1[0], p2[0]):
+                plotmatrix[i][bres_check(m*i+c)]=1
+                plt.plot(i, bres_check(m*i+c), 'g.')
 
         #Case positive slop and slope > 1 (plotting is the same)
         #case printed in red
         if (m>1.0):
             if(m==999.0):
-                print("vertical line")
+                #print("vertical line")
                 #----Make sure always plotting from bottom to top-----
                 if(p1[1]>p2[1]):
                     temp = p2
                     p2 = p1
                     p1 = temp
                 #______________________________________________________
-                step = np.arange(p1[1], p2[1], 1)
-                plt.plot(step*0 + p1[0], step, 'r,')
+                #step = np.arange(p1[1], p2[1], 1)
+                for i in range (p1[1], p2[1]):
+                    plotmatrix[p1[0]][i]=1
+                    plt.plot(i*0+p1[0], i, 'r.')
             else:
-                step = np.arange(p1[1], p2[1], 1)
-                plt.plot((step-c)/m, step, 'r,')
+                #step = np.arange(p1[1], p2[1], 1)
+                for i in range (p1[1], p2[1]):
+                    plotmatrix[bres_check((i-c)/m)][i]=1
+                    plt.plot(bres_check((i-c)/m), i, 'r.')
 
         #Case negative slope and slope < -1 (plotting is the same)
         #case printed in blue
         if (m<-1.0):
-            step = np.arange(p2[1], p1[1], 1)
-            plt.plot((step-c)/m, step, 'b,')
+            #step = np.arange(p2[1], p1[1], 1)
+            for i in range (p2[1], p1[1]):
+                plotmatrix[bres_check((i-c)/m)][i]=1
+                plt.plot(bres_check((i-c)/m), i, 'b.')
 
 
+def flood_fill(start_pointx, start_pointy):
+    global plotmatrix
+    global floodfillcalls
+    floodfillcalls += 1 
+    print floodfillcalls
 
+    if (plotmatrix[start_pointx][start_pointy] == 0):
+        plotmatrix[start_pointx][start_pointy] = 1
+        plt.plot(start_pointx, start_pointy, 'c,')
 
+        #UP
+        flood_fill(start_pointx, start_pointy+1)
+        #Down
+        flood_fill(start_pointx, start_pointy-1)
+        #Left
+        flood_fill(start_pointx-1, start_pointy)
+        #Right
+        flood_fill(start_pointx+1, start_pointy)
+    else:
+        return 0
+        
 
 
 
 
 def convex_hull(points):
-
 
     # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
     # Returns a positive value, if OAB makes a counter-clockwise turn,
@@ -106,6 +143,7 @@ def convex_hull(points):
 
 
 def main():
+    global plotmatrix
     points = []
    
     #__________OPENING A FILE WITH ROBUST CHECKING_____________
@@ -143,6 +181,12 @@ def main():
                 check=0
         print 'Points:\n', points
         plotting_points_into_shape(points)
+        #Find a point in the hull to start filling from
+        startx = ((points[2][0]+points[0][0])/2)
+        starty = ((points[2][1]+points[0][1])/2)
+        #while (plotmatrix[startx][starty]==0):            
+        flood_fill(startx, starty)
+        #startx-=1
 
 
 
@@ -165,7 +209,6 @@ def main():
         #Make sure there is more that 1 point
         if len(points) <= 1:
             return 0
-        plotting_points_into_shape(points)
         points = convex_hull(points)
         plotting_points_into_shape(points)
 
@@ -175,7 +218,6 @@ def main():
 
     plt.axis([0, 1100, 0, 1100])
     plt.show()
-    
 
 main()
             
