99fc1ae740479599eb562026fd5e31f402e920db
Jarvis Schultz <schultzjarvis@gmail.com>
2015-10-26 14:27:52 -0500

    Simple OpenCV version working.

--- image_test.py
+++ image_test.py
@@ -2,9 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
-import PIL
-
+import cv2
 
 
 def bresenham_get_points(x1,y1,x2,y2):
@@ -53,46 +51,66 @@ def bresenham(pt1, pt2, arr):
     return arr
 
 
+
+def flood_fill_UL_nonrecurse(start_pointx, start_pointy, edgearray):
+    fill_set = set()
+    fill_set.add((start_pointx, start_pointy))
+    fill_array = edgearray.copy()
+    while fill_set:
+        (x,y) = fill_set.pop()
+        if not fill_array[x][y]:
+            fill_set.add((x,y+1))
+            fill_set.add((x,y-1))
+            fill_set.add((x-1,y))
+            fill_set.add((x+1,y))
+            fill_array[x][y] = 1
+    return fill_array
+
+
+
 # Let's define a series of points
 points = np.array([
-    [50,50],
-    [100,50],
-    [100,75],
-    [50,75]
+    [150,300],
+    [450,300],
+    [570,525],
+    [653,780],
+    [495,845],
+    [350,740],
+    [100,345]
     ])
 points = np.vstack((points, points[0]))
 
 
-# Get image for polygon:
-poly_arr = np.zeros((150,150), dtype=np.uint8)
+# get array that represents edges of the polygon::
+size = int(np.max(points)*(1.25))
+poly_arr = np.zeros((size,size), dtype=np.uint8)
 for i,p in enumerate(points[:-1]):
     bresenham(p, points[i+1], poly_arr)
 
+# get array for fill:
+fill_arr = flood_fill_UL_nonrecurse(size>>1, size>>1, poly_arr)
 
+# convert poly array:
+poly_rgb = np.zeros((poly_arr.shape[0], poly_arr.shape[1], 3), dtype=np.uint8)
+poly_rgb[:,:,0] = 128
+poly_rgb = cv2.bitwise_and(poly_rgb, poly_rgb, mask=poly_arr)
+
+# convert fill array
+fill_rgb = np.zeros((fill_arr.shape[0], fill_arr.shape[1], 3), dtype=np.uint8)
+fill_rgb[:,:,1:] = 128
+fill_rgb = cv2.bitwise_and(fill_rgb, fill_rgb, mask=fill_arr)
+
+# combine images:
+plot_img = cv2.add(poly_rgb, fill_rgb)
+
+# show image:
 plt.hold(True)
-plt.plot(points[:,0], points[:,1], 'r-o')
-arr = np.swapaxes(arr, 0, 1)
-plt.imshow(arr, cmap='gray_r', alpha=0.2, zorder=-10, origin='lower')
+plot_img = np.swapaxes(plot_img, 0, 1)
+plt.imshow(plot_img, zorder=-10, origin='lower', interpolation='none')
 ax = plt.gca()
-ax.set_xlim([0,150])
-ax.set_ylim([0,150])
+ax.set_xlim([0,size])
+ax.set_ylim([0,size])
 plt.hold(False)
 plt.show()
 
-
-
-
-# arr = np.zeros((150,150), dtype=np.uint8)
-# arr[0:60, 20:40] = 1
-
-
-# plt.hold(True)
-# plt.plot(points[:,0], points[:,1], 'r-o')
-# arr = np.swapaxes(arr, 0, 1)
-# plt.imshow(arr, cmap='gray_r', alpha=0.2, zorder=-10, origin='lower')
-# ax = plt.gca()
-# ax.set_xlim([0,150])
-# ax.set_ylim([0,150])
-# plt.hold(False)
-# plt.show()
 
